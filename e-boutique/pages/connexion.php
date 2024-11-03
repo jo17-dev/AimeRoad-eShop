92c41dc074fb5b65f3e5fe8cd9646af69eb526a1
2372559 <2372559@crosemont.qc.ca>
2024-11-03 18:45:57 -0500

    Ajout des fichiers CSS et PHP pour inscription et connextion

---
+++ e-boutique/pages/connexion.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Se Connecter - AimeRaud</title>
+    <link rel="stylesheet" href="../css/connexion.css">
+</head>
+<body>
+
+    <div class="logo">
+    <img src="../img/logo-320x60.png" alt="">
+    </div>
+
+    <div class="texte">
+    <h1>Connexion</h1>
+    </div>
+
+    <div class="infos">
+    <div></div>
+    <form action="" method="post">
+
+        <div class="info">
+        <label for="adresse">Adresse courriel</label><br>
+        <input type="email" name="adresse" id="adresse" required><br>
+        </div>
+
+        <div class="info">
+        <label for="motDePasse">Mot de passe</label><br>
+        <input type="password" name="motDePasse" id="motDePasse" required><br>
+        </div>
+
+        <div class="info">
+        <a href="./inscription.php" title="Créer un nouveau compte AimeRaud">S'inscrire</a><br>
+        </div>
+
+        <div class="info" id="bouton">
+        <input type="submit" value="Se connecter">
+        </div>
+
+    </form>
+    <div></div>
+    </div>
+
+</body>
+</html>
