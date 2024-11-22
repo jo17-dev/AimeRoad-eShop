5c707a6ab47a5142a3a5081eb5dd790bf8311ef0
jo17-dev <procompter1@gmail.com>
2024-11-22 14:13:34 -0500

    update create_data_base.sql

--- autres/sql/create_data_base.sql
+++ autres/sql/create_data_base.sql
@@ -26,12 +26,13 @@ CREATE TABLE Client (
 -- Table Produit
 CREATE TABLE Produit (
     id INT AUTO_INCREMENT PRIMARY KEY,  -- Clé primaire
-    idCategorie INT,                    -- Référence à la table Categorie
+    idCategorie INT NOT NULL,                    -- Référence à la table Categorie
     nom VARCHAR(255) NOT NULL,           -- Le nom du produit ne peut pas être nul
     prixUnitaire DECIMAL(10, 2) NOT NULL, -- Le prix unitaire doit être non nul et précis à 2 décimales
+    urlPhoto VARCHAR(255) NULL, -- url de la photo
     date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     date_modification TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    CONSTRAINT FK_Produit_Categorie FOREIGN KEY (idCategorie) REFERENCES Categorie(id) 
+    CONSTRAINT FK_Produit_Categorie FOREIGN KEY (idCategorie) REFERENCES Categorie(id)
 );
 
 -- Table Panier
@@ -42,12 +43,12 @@ CREATE TABLE Panier (
     date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     date_modification TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (idClient, idProduit),    -- La clé primaire est une combinaison de client et produit
-    CONSTRAINT FK_Panier_Client FOREIGN KEY (idClient) REFERENCES Client(id) 
+    CONSTRAINT FK_Panier_Client FOREIGN KEY (idClient) REFERENCES Client(id) ,
     CONSTRAINT FK_Panier_Produit FOREIGN KEY (idProduit) REFERENCES Produit(id)
 );
 
 -- Table Administrateur
 CREATE TABLE Administrateur (
-    idClient INT PRIMARY KEY,            -- L'idClient fait office de clé primaire
+    idClient INT UNIQUE,            -- L'idClient fait office de clé primaire
     FOREIGN KEY (idClient) REFERENCES Client(id)
 );
