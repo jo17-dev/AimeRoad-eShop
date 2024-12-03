523f9323c0b332a117a6ee7f055b720692ec8dd0
jo17-dev <procompter1@gmail.com>
2024-12-03 16:33:22 -0500

    changements de la branche de sami

---
+++ e-boutique/vues/includes/header.php
@@ -0,0 +1,16 @@
+<nav class="nav-bar">
+    <img src="img/logo-320x60.png" class="logo" alt="Aimeroad">
+    <ul class="links">
+        <li><a href="?action=voirAcceuil">Accueil</a></li>
+        <li><a href="?action=voirAcceuil#about">À propos</a></li>
+        <li><a href="">Contact</a></li>
+    </ul>
+    <ul class="user-panel">
+        <li>
+            <a href="?action=voirPanier" title="click to view your cart"><img src="img/panier-320x320.png" alt="login"></a>
+        </li>
+        <li>
+            <a href="?action=voirConnexion" title="click to login or connect"><img src="img/compte-320x320.png" alt="login"></a>
+        </li>
+    </ul>
+</nav>
