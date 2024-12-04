bc36a98bed211a41e9c7155d097e7e1449093aac
jo17-dev <procompter1@gmail.com>
2024-12-03 20:33:05 -0500

    update de la vue liste produit admin

--- autres/sql/create_data_base.sql
+++ autres/sql/create_data_base.sql
@@ -30,7 +30,6 @@ CREATE TABLE Produit (
     nom VARCHAR(255) NOT NULL,           -- Le nom du produit ne peut pas être nul
     quantite INT NOT NULL,           -- Le nom du produit ne peut pas être nul
     prixUnitaire DECIMAL(10, 2) NOT NULL, -- Le prix unitaire doit être non nul et précis à 2 décimales
-    quantite INT NOT NULL,
     urlPhoto VARCHAR(255) NULL, -- url de la photo
     date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     date_modification TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
--- e-boutique/vues/listeproduits_admin.php
+++ e-boutique/vues/listeproduits_admin.php
@@ -1,55 +1,83 @@
+<?php
+include_once "models/administrateur.class.php";
+
+if(!is_null($controleur) && isset($_SESSION['utilisateurConnecte']['estAdmin'])){
+    $user = new Administrateur($_SESSION['utilisateurConnecte']['id'], $_SESSION['utilisateurConnecte']['nom'], $_SESSION['utilisateurConnecte']['prenom'], $_SESSION['utilisateurConnecte']['email'], $_SESSION['utilisateurConnecte']['password']);
+
+/**
+ * cette vue n'étant reservée qu'as la "vue", les autres actions sur cette page comme la modification des
+ * infos admins, tout autre seras rédirigé vers l'action
+ */
+    if(isset($_GET['action'])){
+        if($_GET['action'] != "voirListeProduits"){
+            header("Location: ?action=voirListeProduits");
+        }
+    }
+
+}else{
+    header("Location: ?action=''");
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/init.css">
-    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/compte.css">
+    <link rel="stylesheet" href="css/popup.css">
     <link rel="stylesheet" href="css/liste_produits_admin.css">
-    <title>Ameroad - liste des produits</title>
+    <title>Aimeroad</title>
 </head>
 <body>
     <?php include_once("vues/includes/header.php")?>
 
-    <h1 class="title">États des stocks:</h1>
-    <table>
-        <thead>
-            <td>code</td>
-            <td>categorie</td>
-            <td>prix unitaire</td>
-            <td>Quantitée disponible</td>
-            <td>dernière mise à jour</td>
-        </thead>
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>Bijoux</td>
-                <td>250 $</td>
-                <td>0</td>
-                <td>2024-11-22 14:49:45</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Bijoux</td>
-                <td>250 $</td>
-                <td>0</td>
-                <td>2024-11-22 14:49:45</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Bijoux</td>
-                <td>250 $</td>
-                <td>0</td>
-                <td>2024-11-22 14:49:45</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Bijoux</td>
-                <td>250 $</td>
-                <td>0</td>
-                <td>2024-11-22 14:49:45</td>
-            </tr>
-        </tbody>
-    </table>
+    <section class="main-container">
+        <?php include_once("vues/includes/admin_side_nav.include.php") ?>
+        <div class="user-info">
+            <h1>Liste des produits.</h1>
+            <section class="controls-container">
+                <a class="wrapper" href="?action=voirAjouterProduit"><button class="btn-rights add-btn" >Ajouter un produit</button></a>
+            </section>
+            <table>
+                <thead>
+                    <td>code</td>
+                    <td>categorie</td>
+                    <td>prix unitaire </td>
+                    <td>Quantitée disponible</td>
+                    <td colspan="2" >Actions</td>
+                </thead>
+                <tbody>
+                    <?php
+                        if(count($controleur->getTabProduits()) == 0){
+                            echo "<tr><td colspan='5' class='no-datas-warning'>Aucune donnée disponible pour cet affichage</td></tr>";
+                        }else{
+                            foreach($controleur->getTabProduits() as $compte){
+                    ?>
+                    <tr>
+                        <td> <?php echo $compte['id'] ?> </td>
+                        <td><?php echo $compte['nom'] ?></td>
+                        <td><?php echo $compte['prixUnitaire'] . "$" ?></td>
+                        <td><?php echo $compte['quantite'] ?></td>
+                        <td>
+                            <a class="wrapper" href="?action=surprimerDroitsAdmin&idClient=<?php echo $compte['id'] ?>"><button class="btn-rights" >Modifier</button></a>
+                        </td>
+                        <td>
+                        <a class="wrapper" href="?action=suprimerCompte&idClient=<?php echo $compte['id'] ?>" >
+                            <button class="btn-rights btn-delete-account" >Supprimer</button>
+                        </a>
+                        </td>
+                    </tr>
+                    <?php
+                            }
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </section>
+    <script src="js/popup.module.js"></script>
+
 </body>
 </html>
