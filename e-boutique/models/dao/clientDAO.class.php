f86f2a033f2f356d5e9b24af32a99e6eab91a6c7
jo17-dev <procompter1@gmail.com>
2024-12-18 17:29:38 -0500

    supression de la 2e methode supprimer

--- e-boutique/models/dao/clientDAO.class.php
+++ e-boutique/models/dao/clientDAO.class.php
@@ -122,24 +122,6 @@ class ClientDAO implements DAO {
 
         return $rows;
     }
-
-    public static function supprimer(int $id) : bool{
-        try { 
-            $connexion=ConnexionBD::getInstance();   
-        } catch (Exception $e) {
-            return false;
-        }
-        $commande = $connexion->query("SELECT * FROM client WHERE id = $id");
-
-        $rows = $commande->fetchAll();
-        if(count($rows) != 0){ // On ne supprime les droits d'admin que s'il existe déjas
-            $commande = $connexion->exec("DELETE FROM client WHERE id=$id");
-            ConnexionBD::close();
-            return true;
-        }
-        ConnexionBD::close();
-        return false;
-}
 }
 
 ?>
