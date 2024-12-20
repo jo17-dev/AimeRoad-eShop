9306a3d71104e4f03cf50eb4a33134788bf9a314
Daryl <daryl_889@outlook.com>
2024-12-20 00:48:11 -0500

    Derniere modif Daryl

--- e-boutique/vues/listeproduits.php
+++ e-boutique/vues/listeproduits.php
@@ -13,6 +13,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page de magasinage</title>
     <link rel="stylesheet" href="css/listeproduits.css">
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
+    <script>
+    /*
+Notions indispensables
+
+Je vais ici vous parler de deux notions indispensables : les fonctions de rappel et le chaînage de méthodes.
+Fonctions de rappel
+
+Une fonction de rappel (ou callback en anglais) est une fonction exécutée
+lorsqu'une autre fonction a terminé de s'exécuter.
+En jQuery, les fonctions de rappel sont essentiellement utilisées
+pour réaliser des animations et des appels AJAX.
+Nous reviendrons sur ces deux sujets dans la partie 3 du cours.
+
+Juste pour vous mettre l'eau à la bouche, voici
+ un exemple de fonction de rappel,
+appliquée aux éléments de classe rouge :
+	
+*/
+
+    $(function () {
+      
+      $(".boutton-add-panier").hide();
+      $(".boutton-add-panier").fadeIn(700);
+
+    });
+  </script>
+
 </head>
 <body>
    <!--Barre de navigation sur toutes les pages-->
@@ -39,22 +68,15 @@
             <div class="products-grid">
             <?php foreach ($controleur-> getTabProduits() as $produit){ ?>
                 <div class="boite_produit">
-                    <img src="img/produit-demo.jpg" title="image produit" alt="image du produit" class="img-produits" >
+                    <img src="<?php echo $produit -> getUrlPhoto() ?>" title="image produit" alt="image du produit" class="img-produits" >
                     <h2 class="titre_du_produit"><?php echo $produit->getNom()  ?> </h2>
                     <p class="description-du-produit"> <?php echo $produit -> getCategorie() ?></p>
-                    <div class="prix-produit"> <?php echo $produit -> getPrix() ?></div>
-                    <button class="boutton-add-panier"> <a class="btn-content" href="?action=ajouterPanier">Ajouter au panier</a> </button>
+                    <div class="prix-produit"> <?php echo $produit -> getPrixUnitaire() . "$" ?></div>
+                    <button class="boutton-add-panier"><a class="btn-content" href="?action=ajouterPanier">Ajouter au panier</a> </button>
                 </div>
                 <?php } ?>
             </div>
     </div>
-</section>
-<section class="section about-section">
-    <p>Sur notre boutique en ligne, nous offrons à nos clients une panoplie de choix tendance 
-        à travers quatre catégories d’accessoires : manteaux, chapeaux, bijoux, et chaussures. Le nom de notre boutique fait référence au choix unique 
-        de la couleur de nos articles, exclusivement en émeraude, ce magnifique vert qui rappelle également un précieux joyau. 
-        Nous vous invitons à emprunter cette merveilleuse route vers une expérience de magasinage inoubliable sur notre site, 
-        que vous allez certainement aimer!</p>
 </section>
 <!-- footer part -->
     <?php include_once("vues/includes/footer.php")?>
